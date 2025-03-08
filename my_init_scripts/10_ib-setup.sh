5ad2d2d583d1ed3e3b4cf19f5e1f3dc69ee1f365
laur <laur.aliste@gmail.com>
2025-03-08 19:56:48 +0100

    ib-setup: do not move ibgateway/ from root/ to ~
    - note this also implies we gotta set "ib-tws-dir" value in
      Launcher/config.json to "/root/ibgateway"
    - i.e. ibgw is ran from under /root

--- my_init_scripts/10_ib-setup.sh
+++ my_init_scripts/10_ib-setup.sh
@@ -17,34 +17,18 @@ fi
 
 update_lean_links.sh  "${LEAN_TARGET:-Debug}"
 
-ROOT_IBG_DIR='/root/ibgateway'
-IBG_DIR="/home/$USERNAME/ibgateway"
+ROOT_IBG_DIR='/root/ibgateway'  # note Launcher/config.json ib-tws-dir value should be same: "/root/ibgateway"
 
-
-if ! [[ -d "$IBG_DIR" ]]; then
-    if ! [[ -d "$ROOT_IBG_DIR" ]]; then
-        echo "[$ROOT_IBG_DIR] not a dir"
-        exit 1
-    elif ! find "$ROOT_IBG_DIR" -mindepth 1 -maxdepth 1 -print -quit | grep -q .; then
-        echo "[$ROOT_IBG_DIR] is empty"  # sanity
-        exit 1
-    fi
-
-    mkdir -p -- "$IBG_DIR"
-    #mv "$ROOT_IBG_DIR"/* "$IBG_DIR/"  # glob doesn't expand hidden files by default, best move w/ find:
-    find "$ROOT_IBG_DIR/" -maxdepth 1 -mindepth 1 -exec mv -t "$IBG_DIR" {} +
-
-    # perms:
-    #chown "root:$USERNAME"  /root  # TODO: shouldn't/couldn't this be  /root/Jts/  instead of whole /root?; NOPE - cannot be just /Jts; edit: should not be needed if our regular user is in 'root' group
-    chmod -R g+rw              /root/Jts/
-
-    ## note this will disable root ssh login, as /root pers can be max 750; see https://askubuntu.com/a/566418/1002165 :
-    #chmod -R g+rwx /root   # looks like this is required so it can write (into) /root/jts.ini?
-    chmod -R g+rx /root   # this will preserve ssh-as-root capability, but we get no write in /root
-
-    # to find hard-coded occurrences or ibgateway path under /root, do $ cd ~/ibgateway && ffstr '/root/ibgateway'
-    grep --recursive --files-with-matches /root/ibgateway  "$IBG_DIR" \
-        | xargs --no-run-if-empty -- sed -i --follow-symlinks "s+/root/ibgateway+${IBG_DIR}+g"
+if ! [[ -d "$ROOT_IBG_DIR" ]]; then
+    echo "[$ROOT_IBG_DIR] not a dir"
+    exit 1
+elif ! find "$ROOT_IBG_DIR" -mindepth 1 -maxdepth 1 -print -quit | grep -q .; then
+    echo "[$ROOT_IBG_DIR] is empty"  # sanity
+    exit 1
 fi
+
+# perms:
+chmod -R g+rX   /root   # this will preserve ssh-as-root capability, but we get no write in /root itself
+chmod -R g+w    /root/{Jts,ibgateway}/
 
 exit 0
